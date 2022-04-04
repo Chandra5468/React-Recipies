0ab93529d19b77c27e0a9c7d4de1a9e3fbc9e5de
Chandra5468 <agirchandra@gmail.com>
2022-04-05 01:27:52 +0530

    All files cleared

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom"; //useParams allows us to pull up keyword from parameter
+function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type); // .type because we used /:type in Pages.jsx for dynamic url typing/searching.
+    console.log(params); // So using useParams() , params will note/ take the entire url ; so using params.type will give the last url bit of .type used in Pages.jsx
+  }, [params.type]);
+
+  return (
+    <Grid>
+      {cuisine.map((item)=>{
+          return(
+              <Card key={item.id}>
+                <Link to={'recipe/' + item.id}>
+                  <img src={item.image} alt={item.title} />
+                  <h4>{item.title}</h4>
+                  </Link>
+              </Card>
+          )
+      })}
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+export default Cuisine;
